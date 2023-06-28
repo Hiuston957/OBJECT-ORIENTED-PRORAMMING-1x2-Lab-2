a61d67bdb9abc81c17f6280af0a666ebd5bed34b
Luke <lukasz.kutynski@microsoft.wsei.edu.pl>
2023-06-28 22:22:25 +0200

    Add project files.

---
+++ Exercise 6 – LINQ/Program.cs
@@ -0,0 +1,44 @@
+﻿/*LINQ (Language INtegrated Query) is a built-in query syntax for C# to retrieve data from
+various sources and formats. For example, we can use it to work with collections.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Ex_02_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 50, 13, 11, 3, 24, 9, 49 };
+            var animals = new List<string>() { "cat", "horse", "tiger", "mouse" };
+            // Get numbers in order
+            var ordered = from x in numbers
+                          orderby x ascending
+                          select x;
+            // Select only even numbers and divide them by 2
+            var divided = from x in numbers
+                          where x % 2 == 0
+                          select x / 2;
+            // Choose only animals with 'o'
+            var onimols = from s in animals
+                          where s.Contains('o')
+                          select s;
+            // Choose only animals with 5 letters and get them in a descending order
+            var fives = from s in animals
+                        where s.Length == 5
+                        orderby s descending
+                        select s;
+            // Printing
+            ordered.ToList().ForEach(n => Console.WriteLine(n));
+            Console.WriteLine();
+            divided.ToList().ForEach(n => Console.WriteLine(n));
+            Console.WriteLine();
+            onimols.ToList().ForEach(n => Console.WriteLine(n));
+            Console.WriteLine();
+            fives.ToList().ForEach(n => Console.WriteLine(n));
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Exercise 7 – Delegates. Multicast delegates/Program.cs
@@ -0,0 +1,53 @@
+﻿/*A delegate is a reference type that can hold a reference to a method.Think of it as a box,
+inside which you put a method. When you run the delegate, the method inside is called.
+We can also reference (add) many methods within a single delegate. When you run such a
+delegate, all its methods will be run, in the same order as they were added.However, only
+the last method in the delegate will provide the return result.
+*/
+
+using System;
+namespace Ex_02_07
+{
+    class Program
+    {
+        // Declaration of a delegate which takes two ints and returns int
+        // You can set this delegate to any method that takes two ints and returns int
+        delegate int MathOperation(int a, int b);
+        static void Main(string[] args)
+        {
+            // Create delegate instance
+            MathOperation del = Method1; // or = new MathOperation(Method1)
+            del(1, 2); // or del.Invoke(1, 2);
+                       // Set to another method
+            del = Method2;
+            del(8, 5);
+            // Set to a lambda expression
+            del = (x, y) => x - y;
+            int result = del(10, 15);
+            Console.WriteLine(result);
+            /**************************************************/
+            // Multicast delegate
+            del = Method1; // set 1st method
+            del += Method1; // add 1st method again
+            del += Method2; // add 2nd method
+                            // del -= Method1;
+            Console.WriteLine("----------------");
+            // Run the multicast delegate and check its return output
+            result = del(5, 10);
+            Console.WriteLine("Multicast output: " + result);
+        }
+        /******************************************************************/
+        static int Method1(int x, int y)
+        {
+            int z = x + y;
+            Console.WriteLine(z);
+            return z;
+        }
+        static int Method2(int x, int y)
+        {
+            int z = x * y;
+            Console.WriteLine(z);
+            return z;
+        }
+    }
+}
